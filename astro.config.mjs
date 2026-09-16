612e06de86f7373885eca2adab08b94a5fc798a4
Clément Corbin <corbin.clement@gmail.com>
2026-09-16 15:15:12 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { satteri } from '@astrojs/markdown-satteri';
-import { rewriteAssetImages } from './src/lib/rewrite-asset-images.mjs';
+import { defineConfig } from 'astro/config'
+import { satteri } from '@astrojs/markdown-satteri'
+import { rewriteAssetImages } from './src/lib/rewrite-asset-images.mjs'
 
 export default defineConfig({
-	markdown: {
-		processor: satteri({ mdastPlugins: [rewriteAssetImages] }),
-	},
-});
+  site: 'https://www.collectiflartscene.com',
+  markdown: {
+    processor: satteri({ mdastPlugins: [rewriteAssetImages] })
+  }
+})
